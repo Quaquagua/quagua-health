530f92a82ebc1e89302a28ff29a78bae60958954
Quaquagua <70441363+Quaquagua@users.noreply.github.com>
2026-02-12 22:41:00 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
-      <body>{children}</body>
+      <body className="antialiased">{children}</body>
     </html>
   );
 }
