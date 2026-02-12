b35ca428acd2cd9a7cae15b30cfd50db56dd5f6d
Quaquagua <70441363+Quaquagua@users.noreply.github.com>
2026-02-12 22:41:50 +0900

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,7 @@
 module.exports = {
   content: ['./app/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {
-      colors: {
-        border: 'hsl(var(--border))',
-        background: 'hsl(var(--background))',
-        foreground: 'hsl(var(--foreground))',
-        primary: { DEFAULT: 'hsl(var(--primary))', foreground: 'hsl(var(--primary-foreground))' },
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
