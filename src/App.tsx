f5cd181c4d8491ceb8e613a6dc68f4ab1cc0ea65
Quaquagua <70441363+Quaquagua@users.noreply.github.com>
2026-02-14 23:22:21 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import Header from './sections/Header';
 import Hero from './sections/Hero';
+import ProductRecommendations from './sections/ProductRecommendations'; // 위치 이동
 import CategorySection from './sections/CategorySection';
 import FeaturedPosts from './sections/FeaturedPosts';
-import ProductRecommendations from './sections/ProductRecommendations';
 import Newsletter from './sections/Newsletter';
 import Footer from './sections/Footer';
 
@@ -12,9 +12,9 @@ function App() {
       <Header />
       <main>
         <Hero />
+        <ProductRecommendations /> {/* Hero 다음으로 이동 - 제품 먼저 노출! */}
         <CategorySection />
         <FeaturedPosts />
-        <ProductRecommendations />
         <Newsletter />
       </main>
       <Footer />
